640afab51af0ba89baa15e63b2f4c66556c99342
Alan <alannavaseg_02@hotmail.com>
2020-04-18 16:54:28 -0500

    Se crea la base de datos

--- app.py
+++ app.py
@@ -5,9 +5,46 @@ import os
 #Init app
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg':'hello world'})
+basedir = os.path.abspath(os.path.dirname(__file__))
+#Database
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#INIT DB
+db = SQLAlchemy(app)
+#INit Marshmallow
+ma = Marshmallow(app)
+
+#Product Class/Model
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique = True )
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+#Product schem
+
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
+#Init Schema
+
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+
 #Run server
 if __name__ == '__main__':
     app.run(debug=True)
